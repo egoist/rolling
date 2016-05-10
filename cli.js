2837a8d3e38ee351dafcf239bcf156acc5d92458
EGOIST <0x142857@gmail.com>
2016-05-10 12:19:05 +0800

    clean

--- cli.js
+++ cli.js
@@ -3,18 +3,6 @@
 const cac = require('cac')
 const build = require('./lib/build')
 
-cli
-  .command('build')
-  .option('-e, --entry [entry]', 'Entry file')
-  .option('-f, --format [format]', 'Bundle format')
-  .option('-n, --name [moduleName]', 'Bundled module name')
-  .option('-d, --dest [dest]', 'Dest path')
-  .option('-i, --inline', 'Inline required modules')
-  .option('-s, --skip', 'Never inline these modules')
-  .option('-j, --jsnext', 'Resolve jsnext field when inline modules')
-  .option('--babelrc', 'User local babelrc file')
-  .action(build)
-
 const cli = cac(`
   Usage:
     rolling [file] [options]
